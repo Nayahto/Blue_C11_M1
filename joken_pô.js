400ee1f5e6a532414d29f0461dfa8b4cffd5436c
Nayahto <91474853+Nayahto@users.noreply.github.com>
2022-01-30 17:27:26 -0300

    Add files via upload

---
+++ joken_pô.js
@@ -0,0 +1,92 @@
+let cont = +prompt (`diga quantas rodadas voce quer jogar`)
+let selec_item
+let a = Math.floor(Math.random(0,3)*3);
+let vet = [`pedra`, `papel`,`tesoura`]
+let d = vet[a]
+let comp = 0;
+let play = 0;
+let jog_n
+let jog_nov
+while(cont > 0 || jog_nov > 0)
+{    jog_nov --
+     cont --
+      selec_item = prompt (`faca uma escolha entre pedra, papel ou tesoura`)
+    if (d == `pedra` && selec_item == `pedra`){
+          console.log(`seu oponente escolheu: ${d}\n você escolheu ${selec_item}\n resultado: empate`)
+    }
+    if (d == `pedra` && selec_item == `papel`){
+          console.log(`seu oponente escolheu: ${d}\n você escolheu ${selec_item}\n resultado: voce venceu`)
+          play ++
+    }
+    if (d == `pedra` && selec_item == `tesoura`){
+          console.log(`seu oponente escolheu: ${d}\n você escolheu ${selec_item}\nresultado: voce perdeu`)
+          comp ++
+    }
+    if (d == `papel` && selec_item == `pedra`){
+          console.log(`seu oponente escolheu: ${d}\n você escolheu ${selec_item} \nresultado: voce perdeu`)
+          comp ++
+    }
+    if (d == `papel` && selec_item == `papel`){
+          console.log(`seu oponente escolheu: ${d}\n você escolheu ${selec_item}\n resultado: empate`)
+    }
+    if (d == `papel` && selec_item == `tesoura`){
+          console.log(`seu oponente escolheu: ${d}\n você escolheu ${selec_item}\n resultado: voce venceu`)
+          play ++
+    }
+    if (d == `tesoura` && selec_item == `pedra`){
+          console.log(`seu oponente escolheu: ${d}\n você escolheu ${selec_item} \nresultado: voce venceu`)
+          play ++
+    }
+    if (d == `tesoura` && selec_item == `papel`){
+          console.log(`seu oponente escolheu: ${d}\n você escolheu ${selec_item}\n resultado: voce perdeu`)
+          comp ++
+    }
+    if (d == `tesoura` && selec_item == `tesoura`){
+          console.log(`seu oponente escolheu: ${d}\n você escolheu ${selec_item}\n resultado: voce empate`)
+    }
+}
+if (play > comp)
+{
+      console.log(`voce venceu ${play} rodadas \n seu oponente venceu ${comp} rodadas\n resultado: voce é o vencedor`)
+      jog_n ++
+      
+      for (let k = 0 ; k == cont; k++){
+      jog_n = prompt(`deseja jogar novamente`)
+      if (jog_n == `sim`){
+            jog_nov = 1
+      }else if(jog_n == `nao`)
+      {
+            console.log(`fim de jogo!`)
+      }
+        
+      }
+      
+}else if (play < comp){
+      console.log(`voce venceu ${play} rodadas\n seu oponente venceu ${comp} rodadas \n resultado: voce perdeu`)
+      jog_n ++
+      for (let k = 0 ; k == cont; k++){
+      jog_n = prompt(`deseja jogar novamente? responda com (sim) ou (nao)`)
+      if (jog_n == `sim`){
+            jog_nov = 1
+      }else if(jog_n == `nao`)
+      {
+            console.log(`fim de jogo!`)
+      }
+        
+      }
+}else if(comp == play){
+      console.log(`empate`)
+      jog_n ++
+      for (let k = 0 ; k == cont; k++){
+      jog_n = prompt(`deseja jogar novamente? responda com (sim) ou (nao)`)
+      if (jog_n == `sim`){
+            jog_nov = 1
+      }else if(jog_n == `nao`)
+      {
+            console.log(`fim de jogo!`)
+      }
+        
+      }
+}else {}
+
+ 
